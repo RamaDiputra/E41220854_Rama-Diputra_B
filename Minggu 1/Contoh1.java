e57de9f6ca675f8da662b2b628fd9adf2bc9b606
ramaDiputra <ramadiputraa88@gmail.com>
2023-02-11 19:56:52 +0700

    Contoh 1

---
+++ Minggu 1/Contoh1.java
@@ -0,0 +1,12 @@
+package contoh1;
+
+public class Contoh1 {
+
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=5;i++){
+            System.out.println("Hello Jember");
+        }
+    }
+    
+}
